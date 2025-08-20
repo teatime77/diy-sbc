5b920d1d722fd9e81c482976fb17e2f628b9b5f3
teatime77 <teatime77@live.jp>
2025-08-20 09:33:45 +0900

    install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "update package index files"
+sudo apt update
+
+echo "upgrade packages"
+sudo apt upgrade
+
+echo "enable ssh server"
+sudo raspi-config nonint do_ssh 0
+
+echo "enable I2C"
+sudo raspi-config nonint do_i2c 0
+
+echo "install camera and opencv"
+sudo apt install -y python3-picamera2 python3-opencv
+
+echo "make a virtual environment of python."
+echo "To avoid mismatches between picamera2 and opencv versions, use the global Python packages with --system-site-packages."
+python3 -m venv ./env --system-site-packages
+
+echo "enter the virtual environment"
+source ./env/bin/activate
+
+echo "install I2C and servo motor libraries"
+pip3 install adafruit-blinka adafruit-circuitpython-pca9685 adafruit-circuitpython-servokit RPi.GPIO
+
+echo "install Flask web server"
+pip3 install Flask
+
+echo "install Joystick libraries"
+pip3 install pygame
+
+echo "install audio libraries"
+pip3 pydub
+
+echo "install Google Cloud Text-to-Speech API"
+pip3 install gtts
